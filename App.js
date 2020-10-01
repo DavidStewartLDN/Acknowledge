a35ad783ba9dc2d2e93c9c9dac0b953c1a3d9b06
DavidStewartLDN <davidstewart.work@gmail.com>
2020-10-01 15:05:41 +0100

    Removed old redux store before trialing new store

--- App.js
+++ App.js
@@ -4,15 +4,15 @@ import { Provider as PaperProvider} from 'react-native-paper';
 import AppNavigator from './src/navigation';
 
 // Imports for redux
-import { Provider as StoreProvider } from 'react-redux'
-import store from './src/redux/store'
+// import { Provider as StoreProvider } from 'react-redux'
+// import store from './src/redux/store'
+
+
 
 export default function App() {
   return (
-    <StoreProvider store={store}>
       <PaperProvider>
         <AppNavigator />
       </PaperProvider>
-    </StoreProvider>
   );
 }
