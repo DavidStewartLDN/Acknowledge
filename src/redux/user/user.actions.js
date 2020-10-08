b461ad3e76d9424c482350863196fddf86e2dfdf
David Stewart <davidstewart.work@gmail.com>
2020-10-08 20:03:47 +0100

    Signup now creates User in database on creation of user

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,4 +1,4 @@
-import Firebase from '../../../config/Firebase.js'
+import Firebase, { db } from '../../../config/Firebase.js'
 
 // define types
 
@@ -40,9 +40,21 @@ export const signup = () => {
       try {
           const { email, password } = getState().user
           const response = await Firebase.auth().createUserWithEmailAndPassword(email, password)
-          dispatch({ type: SIGNUP, payload: response.user })
+          // dispatch({ type: SIGNUP, payload: response.user })
+          if (response.user.uid) {
+            const user = {
+              uid: response.user.uid,
+              email: email
+            }
+
+            db.collection('users')
+              .doc(response.user.uid)
+              .set(user)
+
+            dispatch({ type: SIGNUP, payload: response.user })
+          }
       } catch (e) {
-          console.log(e)
+          alert(e)
       }
   }
 }
