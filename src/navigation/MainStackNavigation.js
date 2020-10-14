d955e89d0f651349af90d0ba6d0a808063f40337
DavidStewartLDN <davidstewart.work@gmail.com>
2020-10-14 20:52:54 +0100

    Added ternary in navigator to choose stack based on uid value - not cleared on logout

--- src/navigation/MainStackNavigation.js
+++ src/navigation/MainStackNavigation.js
@@ -5,9 +5,16 @@ import ViewAchievements from '../screens/ViewAchievements';
 import AddAchievement from '../screens/AddAchievement';
 import GraphAchievements from '../screens/GraphAchievements';
 
+import { useSelector } from 'react-redux'
+import Login from '../screens/Login';
+import Signup from '../screens/Signup';
+import Profile from '../screens/Profile';
+
 const Stack = createStackNavigator()
 
 function MainStackNavigator() {
+  const user = useSelector(state => state.user)
+  console.log(user.uid)
   return (
       <Stack.Navigator
         initialRouteName='ViewAchievements'
@@ -16,18 +23,35 @@ function MainStackNavigator() {
           headerShown: false
         }}
         >
-        <Stack.Screen
-          name='ViewAchievements'
-          component={ViewAchievements}
-        />
-        <Stack.Screen
-          name='AddAchievement'
-          component={AddAchievement}
-        />
-        <Stack.Screen
-          name='GraphAchievements'
-          component={GraphAchievements}
-        />
+        {user.uid == undefined ? (<>
+          <Stack.Screen
+            name='Login'
+            component={Login}
+          />
+          <Stack.Screen
+            name='Signup'
+            component={Signup}
+          />
+          <Stack.Screen
+            name='Profile'
+            component={Profile}
+          />
+        </>) : (
+          <>
+            <Stack.Screen
+              name='ViewAchievements'
+              component={ViewAchievements}
+            />
+            <Stack.Screen
+              name='AddAchievement'
+              component={AddAchievement}
+            />
+            <Stack.Screen
+              name='GraphAchievements'
+              component={GraphAchievements}
+            />
+          </>
+        ) }
       </Stack.Navigator>
   )
 }
