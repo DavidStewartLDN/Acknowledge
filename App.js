fd9015f381522f2dc517a1222dfc7ff57892e3ff
DavidStewartLDN <davidstewart.work@gmail.com>
2020-10-14 19:22:20 +0100

    Added NavigationContainer to App.js

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 import { Provider as PaperProvider} from 'react-native-paper';
+
+// Likely remove App Navigator once finishing updating react-navigation to V.5
 import AppNavigator from './src/navigation';
+
+// Replaces createAppContainer from react-navigation V.4
+import { NavigationContainer } from '@react-navigation/native';
 
 // Imports for redux
 import { Provider } from 'react-redux'
@@ -13,7 +18,9 @@ export default function App() {
   return (
     <Provider store={store}>
       <PaperProvider>
-        <AppNavigator />
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
       </PaperProvider>
     </Provider>
   );
