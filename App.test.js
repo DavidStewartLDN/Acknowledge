7d30a5c301a1263d6280df138d70590c08e46a64
ratcliffetj <ratcliffetj89@gmail.com>
2020-10-16 12:51:35 +0100

    Fixed errors when running Jest tests

--- App.test.js
+++ App.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-
+import { act } from 'react-test-renderer';
 import App from './App';
+import { NavigationContainer } from "@react-navigation/native";
+
+
+jest.useFakeTimers();
+
 
 describe('<App />', () => {
-  it('has 1 child', () => {
+  it('has 1 child', async () => {
     const tree = renderer.create(<App />).toJSON();
-    expect(tree.children.length).toBe(1);
+    await act(async () => { 
+      expect(tree.children.length).toBe(1); 
+    })
   });
 });
