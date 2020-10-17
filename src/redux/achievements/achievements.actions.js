8da87d58faf52321ed63274ef7334c5032557488
DavidStewartLDN <davidstewart.work@gmail.com>
2020-10-17 12:35:40 +0100

    Added update acheivement fields in achievements.actions

--- src/redux/achievements/achievements.actions.js
+++ src/redux/achievements/achievements.actions.js
@@ -18,3 +18,35 @@ export function deleteachievement(id) {
     payload: id
   }
 }
+
+export const updateAchievementTitle = achievementTitle => {
+  return {
+      type: UserActionTypes.UPDATE_ACHIEVEMENT_TITLE,
+      payload: achievementTitle
+  }
+}
+
+export const updateSelectedA = selectedA => {
+  return {
+      type: UserActionTypes.UPDATE_SELECTED_A,
+      payload: selectedA
+  }
+}
+
+export const updateSelectedB = selectedB => {
+  return {
+      type: UserActionTypes.UPDATE_SELECTED_B,
+      payload: selectedB
+  }
+}
+
+export const addAchievementFirebase = () {
+  return async (dispatch, getState) => {
+    try {
+      db.collection("users").doc(user.uid).collection('achievements')
+        .add(achievement)
+    } catch (e) {
+        alert(e)
+    }
+  }
+}
