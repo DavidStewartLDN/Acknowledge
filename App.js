776ea119f8f686fd4f6e89e291b1a937831a4f97
Katie Jones <katieljones29@gmail.com>
2020-10-22 17:11:12 +0100

    add achievement route works from tab bar navigation by nesting navigation routing

--- App.js
+++ App.js
@@ -19,53 +19,66 @@ import Signup from './src/screens/Signup';
 import { NavigationContainer } from '@react-navigation/native';
 import { useSelector } from 'react-redux'
 
-const Stack = createStackNavigator()
+const AuthStack = createStackNavigator()
+const AchievementStack = createStackNavigator()
+const GraphStack = createStackNavigator()
 const Tab = createMaterialBottomTabNavigator();
 
 import { Provider } from 'react-redux'
 
 import store from './src/redux/store'
 
-function MainStackNavigator() {
+function AuthenticationStackScreen() {
   const user = useSelector(state => state.user)
   return (
 
-      <Stack.Navigator
+      <AuthStack.Navigator
         initialRouteName='ViewAchievements'
         screenOptions={{
           gestureEnabled: true,
           headerShown: false
         }}
         >
-        {user.uid == undefined ? (<>
-          <Stack.Screen
+        if user.uid == undefined {
+
+        <>
+          <AuthStack.Screen
             name='Login'
             component={Login}
           />
-          <Stack.Screen
+          <AuthStack.Screen
             name='Signup'
             component={Signup}
           />
-        </>) : (
+        </>}
+        </AuthStack.Navigator>
 
-          <>
-          <Stack.Screen
-              name='ViewAchievements'
-              component={ViewAchievements}
-            />
-          <Stack.Screen
-            name='AddAchievement'
-            component={AddAchievement}
-          />
-            <Stack.Screen
-              name='GraphAchievements'
-              component={GraphAchievements}
-            />
+  ) }
 
-          </>
-        ) }
-      </Stack.Navigator>
-  )
+  function AchievementsStackScreen() {
+    return (
+      <AchievementStack.Navigator>
+      <AchievementStack.Screen
+          name='ViewAchievements'
+          component={ViewAchievements}
+        />
+      <AchievementStack.Screen
+        name='AddAchievement'
+        component={AddAchievement}
+      />
+      </AchievementStack.Navigator>
+    );
+  }
+
+function GraphStackScreen() {
+  return (
+    <GraphStack.Navigator>
+    <GraphStack.Screen
+      name='Graph Achievements'
+      component={GraphAchievements}
+    />
+    </GraphStack.Navigator>
+  );
 }
 
 function App() {
@@ -80,9 +93,9 @@ function App() {
            }}>
            <Tab.Screen
              name="ViewAchievements"
-             component={ViewAchievements}
+             component={AchievementsStackScreen}
              options={{
-               tabBarLabel: 'ViewAchievements',
+               tabBarLabel: 'View Achievements',
                tabBarIcon: ({ color, size }) => (
                  <MaterialCommunityIcons name="home" color={color} size={size} />
                ),
@@ -90,9 +103,9 @@ function App() {
            />
            <Tab.Screen
              name="GraphAchievements"
-             component={GraphAchievements}
+             component={GraphStackScreen}
              options={{
-               tabBarLabel: 'GraphAchievements',
+               tabBarLabel: 'Graph Achievements',
                tabBarIcon: ({ color, size }) => (
                  <MaterialCommunityIcons
                    name="settings"
