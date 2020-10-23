edba6ffa8d05da017e36373348b0155ab12d4e35
ratcliffetj <ratcliffetj89@gmail.com>
2020-10-23 11:52:42 +0100

    Added tests for checkbox component

--- App.test.js
+++ App.test.js
@@ -1,8 +1,13 @@
-import React from 'react';
+/**
+* @jest-environment jsdom
+*/
+
+ import React from 'react';
 import renderer from 'react-test-renderer';
 import { act } from 'react-test-renderer';
 import App from './App';
-import { NavigationContainer } from "@react-navigation/native";
+import Checkbox from './src/components/checkbox';
+import {fireEvent, render, screen} from '@testing-library/react';
 
 
 jest.useFakeTimers();
@@ -15,4 +20,29 @@ describe('<App />', () => {
       expect(tree.children.length).toBe(1); 
     })
   });
+
+  it('renders correctly', async () => {
+    const tree = renderer.create(<App />).toJSON();
+    await act(async () => {
+      expect(tree).toMatchSnapshot();
+    })
+  });
 });
+
+describe('<Checkbox />', () => {
+
+  test('Checkbox is created with appropriate text', () => {
+    const update = jest.fn();
+    const checkbox = render(<Checkbox label="Work" saveSelected={update} />);
+    expect(checkbox.queryByText("Work")).toBeTruthy(); 
+  });
+  
+  test('Checkbox calls supplied function on click', () => {
+    const update = jest.fn();
+    const checkbox = render(<Checkbox label="Work" saveSelected={update} />);
+    fireEvent.click(checkbox.getByText("Work"));
+    expect(update).toHaveBeenCalled();
+  });
+
+});
+
