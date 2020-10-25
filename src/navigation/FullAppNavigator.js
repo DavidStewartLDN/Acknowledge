478edb03c42bef9d73fbac3abbebf338e8362233
DavidStewartLDN <davidstewart.work@gmail.com>
2020-10-25 15:02:56 +0000

    Moved Bottom Tab navigator out of FullAppNavigator

--- src/navigation/FullAppNavigator.js
+++ src/navigation/FullAppNavigator.js
@@ -1,63 +1,13 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack'
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
-
-// Main App Screens
-import ViewAchievements from '../screens/ViewAchievements';
-import AddAchievement from '../screens/AddAchievement';
-import GraphAchievements from '../screens/GraphAchievements';
-import Profile from '../screens/Profile';
 
 import { useSelector } from 'react-redux'
 
-// Importing Navigators
+// Importing Navigation Stacks
 import AuthenticationStack from './AuthenticationStack'
-import AchievementsStack from './AchievementsStack'
+import MainAppBottomTabNavigator from './MainAppBottomTabNavigator'
 
-const AchievementStack = createStackNavigator()
-const Tab = createMaterialBottomTabNavigator();
 const Stack = createStackNavigator()
-
-
-
-
-function MainAppBottomTabNavigator() {
-  return(
-    <Tab.Navigator
-      initialRouteName="ViewAchievements"
-      shifting={true}
-      sceneAnimationEnabled={true}
-      barStyle={{ backgroundColor: '#60DBC5' }}
-      activeColor="#f0edf6"
-      inactiveColor='#2E7166'
-      >
-      <Tab.Screen
-        name="ViewAchievements"
-        component={AchievementsStack}
-        options={{
-          tabBarLabel: 'Achievements',
-          tabBarIcon: 'home',
-        }}
-      />
-      <Tab.Screen
-        name="GraphAchievements"
-        component={GraphAchievements}
-        options={{
-          tabBarLabel: 'Graph',
-          tabBarIcon: 'chart-bar',
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={Profile}
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarIcon: 'account'
-        }}
-      />
-    </Tab.Navigator>
-  )
-}
 
 function FullAppNavigator() {
   const user = useSelector(state => state.user)
