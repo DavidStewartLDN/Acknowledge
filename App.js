8890a62a25ecc44c80df8fc2599f291dfeb354d5
India Rex <face940@gmail.com>
2020-10-27 16:56:59 +0000

    all async functions now in components instead of app.js for push notifications

--- App.js
+++ App.js
@@ -10,11 +10,9 @@ import { Provider } from 'react-redux'
 import store from './src/redux/store'
 
 // Push notifications
-import Constants from 'expo-constants';
 import * as Notifications from 'expo-notifications';
-import * as Permissions from 'expo-permissions';
 import { Text, View, Button, Platform } from 'react-native';
-import { sendPushNotification } from './src/components/pushNotifications'
+import { sendPushNotification, registerForPushNotificationsAsync } from './src/components/pushNotifications'
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -75,36 +73,6 @@ export default function App() {
 // Can use this function below, OR use Expo's Push Notification Tool-> https://expo.io/notifications
 
 
-async function registerForPushNotificationsAsync() {
-  let token;
-  if (Constants.isDevice) {
-    const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-      finalStatus = status;
-    }
-    if (finalStatus !== 'granted') {
-      alert('Failed to get push token for push notification!');
-      return;
-    }
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-    console.log(token);
-  } else {
-    alert('Must use physical device for Push Notifications');
-  }
-
-  if (Platform.OS === 'android') {
-    Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
-  }
-
-  return token;
-}
 
 // function App() {
 //   return (
--- src/components/pushNotifications.js
+++ src/components/pushNotifications.js
@@ -1,8 +1,12 @@
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+import * as Notifications from 'expo-notifications';
+
 export async function sendPushNotification(expoPushToken) {
   const message = {
     to: expoPushToken,
     sound: 'default',
-    title: 'Title Version 2',
+    title: 'Title Version 3',
     body: 'And here is the body!',
     data: { data: 'goes here' },
   };
@@ -16,4 +20,35 @@ export async function sendPushNotification(expoPushToken) {
     },
     body: JSON.stringify(message),
   });
+}
+
+export async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
 }
