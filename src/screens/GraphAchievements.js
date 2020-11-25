e8c65a5735371b9f94df75721a6cf5fc672b409b
Rabee Mghrabi <learningprogramming2019@gmail.com>
2020-11-25 16:19:34 +0000

    added the new chart code

--- src/screens/GraphAchievements.js
+++ src/screens/GraphAchievements.js
@@ -1,66 +1,61 @@
 import React, {useState, useEffect} from 'react';
 import { StyleSheet, View, Dimensions} from 'react-native';
-
+import BarChartVerticalWithLabels from '../components/BarChartVerticalWithLabels'
 import { BarChart } from "react-native-chart-kit";
 
 // Access state in Redux
 import { useSelector } from 'react-redux'
 
 function GraphAchievements() {
-  const achievements = useSelector(state => state.achievements)
-
-  const [count, setCount] = useState([])
-  const partOfLife = ["Work", "Self", "Play", "Living"]
-  // const satisfier = ["Health, Wellbeing, Fitness","Creating","New Developments","Giving"]
-  
-  const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
-  
-  const countLabels = () => {
-    const allData = []
-    const countValues = []
-    var i;
-    for (i = 0; i < achievements.length; i++) {
-      var j;
-      const partOfLifeValues = achievements[i].selectedA
-      for (j = 0; j < partOfLifeValues.length; j++) {
-        allData.push(partOfLifeValues[j])
-      }
-    }
-    var k;
-    for (k = 0; k < partOfLife.length; k++) {
-      countValues.push(countOccurrences(allData,partOfLife[k]))
-    }
-    setCount(countValues)
-  }
-
-  useEffect(() => {
-    countLabels()
-  }, [achievements]);
-
-  const data = {
-    labels: partOfLife,
-    datasets: [{
-      data: count
-    }]
-  }
-
-  const chartConfig = {
-    backgroundGradientFrom: '#1E2923',
-    backgroundGradientTo: '#60DBC5',
-    backgroundColor: '#60DBC5',
-    decimalPlaces: 0,
-    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`
-  }
+  // const achievements = useSelector(state => state.achievements)
+  //
+  // const [count, setCount] = useState([])
+  // const partOfLife = ["Work", "Self", "Play", "Living"]
+  // // const satisfier = ["Health, Wellbeing, Fitness","Creating","New Developments","Giving"]
+  //
+  // const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+  //
+  // const countLabels = () => {
+  //   const allData = []
+  //   const countValues = []
+  //   var i;
+  //   for (i = 0; i < achievements.length; i++) {
+  //     var j;
+  //     const partOfLifeValues = achievements[i].selectedA
+  //     for (j = 0; j < partOfLifeValues.length; j++) {
+  //       allData.push(partOfLifeValues[j])
+  //     }
+  //   }
+  //   var k;
+  //   for (k = 0; k < partOfLife.length; k++) {
+  //     countValues.push(countOccurrences(allData,partOfLife[k]))
+  //   }
+  //   setCount(countValues)
+  // }
+  //
+  // useEffect(() => {
+  //   countLabels()
+  // }, [achievements]);
+  //
+  // const data = {
+  //   labels: partOfLife,
+  //   datasets: [{
+  //     data: count
+  //   }]
+  // }
+  //
+  // const chartConfig = {
+  //   backgroundGradientFrom: '#1E2923',
+  //   backgroundGradientTo: '#60DBC5',
+  //   backgroundColor: '#60DBC5',
+  //   decimalPlaces: 0,
+  //   color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`
+  // }
 
   return (
     <>
       <View style={styles.container}>
-        <BarChart
-          data={data}
-          width={Dimensions.get("window").width-20} // from react-native
-          height={Dimensions.get("window").height-250}
-          chartConfig={chartConfig}
-        />
+        <BarChartVerticalWithLabels/>
       </View>
     </>
   )
