2a13ae36398f93a2508a3e35932a78f9e624e69f
DavidStewartLDN <davidstewart.work@gmail.com>
2020-12-01 15:20:54 +0000

    Added skeleton strucutre of homepage

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,49 +2,49 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Text } from 'react-native-paper';
 import { LinearGradient } from 'expo-linear-gradient';
+import PieChartWithDynamicSlices from '../components/PieChartWithDynamicSlices';
 
 
 function HomeScreen() {
 
+  const achievements = useSelector(state => state.achievements)
+  
+  const [count, setCount] = useState([])
+  const partOfLife = ["Work", "Self", "Play", "Living"]
+  // const satisfier = ["Health, Wellbeing, Fitness","Creating","New Developments","Giving"]
+  
+  const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+  
+  const countLabels = () => {
+    const allData = []
+    const countValues = []
+    var i;
+    for (i = 0; i < achievements.length; i++) {
+      var j;
+      const partOfLifeValues = achievements[i].selectedA
+      for (j = 0; j < partOfLifeValues.length; j++) {
+        allData.push(partOfLifeValues[j])
+      }
+    }
+    var k;
+    for (k = 0; k < partOfLife.length; k++) {
+      countValues.push(countOccurrences(allData,partOfLife[k]))
+    }
+    setCount(countValues)
+  }
+  
+  useEffect(() => {
+    countLabels()
+  }, [achievements]);
+
   return (
     <>
-      <LinearGradient
-        colors={['#60DBC5', '#B2F0E5', '#68EDD5','#70FFE5','#60DBC5']}
-        style={{flex: 1}}
-        //  Linear Gradient 
-        // start={{ x: 0, y: 0 }}
-        // end={{ x: 0, y: 1 }}
-
-        // Linear Gradient Reversed
-        // start={{ x: 0, y: 1 }}
-        // end={{ x: 1, y: 0 }}
-
-        // Horizontal Gradient
-        // start={{ x: 0, y: 0 }}
-        // end={{ x: 1, y: 0 }}
-
-        // Diagonal Gradient
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 1 }}
-      >
       <View style={styles.container}>
         <View style={styles.titleContainer}>
           <Text style={styles.title}>Welcome to Acknowledge</Text>
-
-          <Text style={styles.body}>
-
-          This is a space for you to acknowledge everything you achieve and are proud of each day.
-          There are things you maybe didn’t want to do, but you still did them {"\n"}{"\n"}
-          
-          You can tag each entry to see where it sits and what is covers -
-          get present to everything that you are already doing.{"\n"}{"\n"}
-          
-          No stress, no pressure, you’re doing enough. 
-          Here is where you can see it all and give yourself credit. {"\n"}{"\n"}
-          </Text>
+          </View>
         </View>
-        </View>
-      </LinearGradient>
+      <PieChartWithDynamicSlices />
     </>
   )
 }
