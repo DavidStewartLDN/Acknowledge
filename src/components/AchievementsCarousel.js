c31ac426a849fffd2db2daaebdc962b17d515f38
DavidStewartLDN <davidstewart.work@gmail.com>
2020-12-02 15:04:09 +0000

    Made infinite loop animation work with 5 second delay in middle

--- src/components/AchievementsCarousel.js
+++ src/components/AchievementsCarousel.js
@@ -1,9 +1,14 @@
 import React, { useRef } from "react";
-import { Animated, Text, View, StyleSheet, Button } from "react-native";
+import { Animated,Easing, Text, View, StyleSheet, Button } from "react-native";
 
 const AchievementsCarousel = () => {
+
+  const initialOpacity = 0;
+  const maxOpacity = 1;
+  const standardTime = 4000;
+  const delayTime = 5000;
   // fadeAnim will be used as the value for opacity. Initial Value: 0
-  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const fadeAnim = useRef(new Animated.Value(initialOpacity)).current;
 
   const fadeIn = () => {
     // Will change fadeAnim value to 1 in 5 seconds
@@ -23,6 +28,25 @@ const AchievementsCarousel = () => {
     }).start();
   };
 
+  const infiniteLoop = () => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(fadeAnim, {
+        toValue: maxOpacity,
+        easing: Easing.inOut(Easing.sin),
+        duration: standardTime,
+        useNativeDriver: true
+        }),
+        Animated.delay(delayTime),
+        Animated.timing(fadeAnim, {
+          toValue: initialOpacity,
+          easing: Easing.inOut(Easing.sin),
+          duration: standardTime,
+          useNativeDriver: true
+        }),
+      ])).start();
+  };
+
   return (
     <View style={styles.container}>
       <Animated.View
@@ -38,6 +62,7 @@ const AchievementsCarousel = () => {
       <View style={styles.buttonRow}>
         <Button title="Fade In" onPress={fadeIn} />
         <Button title="Fade Out" onPress={fadeOut} />
+        <Button title="Infinite" onPress={infiniteLoop} />
       </View>
     </View>
   );
