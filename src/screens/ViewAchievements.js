2dfcca2e4282fa39ab40274f828d874ab13c78dc
Matthew Lock <matthew_lock20@hotmail.com>
2020-12-17 15:27:28 +0000

    Start date selector appearing on calendar

--- src/screens/ViewAchievements.js
+++ src/screens/ViewAchievements.js
@@ -14,11 +14,16 @@ import { getachievementsfirebase, deleteachievementfirebase, addachievementfireb
 
 
 function ViewAchievements({ navigation }) {
-  const [selectedDate, setDate] = useState('2020-11-27');
+  //const [selectedDate, setDate] = useState('2020-11-27');
+  const [startDate, setStartDate] = useState('2020-11-27')
   
   const selectDate = (date) => {
     setDate(date.dateString)
   };
+
+  const selectStartDate = (date) => {
+    setStartDate(date.dateString)
+  }
 
   const convertDate = (date) => {
     var day = date.getDate();
@@ -38,10 +43,10 @@ function ViewAchievements({ navigation }) {
   }
 
   // const selectAchievements = (state) => state.achievements.filter(achievement => achievement.createdAt.seconds === seconds);
-  const selectAchievements = (state) => state.achievements.filter(achievement => convertDate(new Date(getMS(achievement.createdAt))) === selectedDate);
+  const selectAchievements = (state) => state.achievements.filter(achievement => convertDate(new Date(getMS(achievement.createdAt))) === startDate);
   const achievements = useSelector(selectAchievements)
   console.log(achievements)
-
+console.log(startDate)
 
   const dispatch = useDispatch()
   const getAchievementsFirebase = () => dispatch(getachievementsfirebase())
@@ -55,8 +60,23 @@ function ViewAchievements({ navigation }) {
   return (
     <>
       <Header titleText='Access' />
-      <Calendar
-        onDayPress={(day) => {selectDate(day)}}
+      <Calendar 
+         //onDayPress={(day) => {selectDate(day)}}
+         onDayPress={(day) => {selectStartDate(day)}}
+         
+    markedDates={{
+    '2020-12-17': {textColor: 'black'},
+    //[startDate]: {startingDay: true, color: 'yellow'},
+    [startDate]: {startingDay: true, color: '#50cebb', textColor: 'white'},
+    '2020-12-22': {color: '#70d7c7', textColor: 'white'},
+    '2020-12-23': {color: '#70d7c7', textColor: 'white', marked: true, dotColor: 'white'},
+    '2020-12-24': {color: '#70d7c7', textColor: 'white'},
+    '2020-12-25': {endingDay: true, color: '#50cebb', textColor: 'white'},
+    //'2020-12-24': {selected: true, endingDay: true, color: 'yellow', textColor: 'gray'},
+    //'2020-12-04': {disabled: true, startingDay: true, color: 'green', endingDay: true}
+  // Date marking style [simple/period/multi-dot/custom]. Default = 'simple'
+    }}
+  markingType={'period'}
       />
       <View style={styles.container}>
         {achievements.length === 0 ? (
